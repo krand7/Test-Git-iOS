8fd86095e7fccfa0ab84afaeb2ae3555f00547e5
Kyle Rand <kylerand@Kyles-MacBook-Pro.local>
2014-08-19 14:05:41 -0400

    Initial Commit

---
+++ Git Test/KPRViewController.h
@@ -0,0 +1,13 @@
+//
+//  KPRViewController.h
+//  Git Test
+//
+//  Created by Kyle Rand on 8/19/14.
+//  Copyright (c) 2014 PracticeCoding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KPRViewController : UIViewController
+
+@end
