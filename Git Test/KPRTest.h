5970622bbd60070acf5ec38b76b39b7500d309c5
Kyle Rand <kylerand@Kyles-MacBook-Pro.local>
2014-08-19 14:18:12 -0400

    added a test class

---
+++ Git Test/KPRTest.h
@@ -0,0 +1,13 @@
+//
+//  KPRTest.h
+//  Git Test
+//
+//  Created by Kyle Rand on 8/19/14.
+//  Copyright (c) 2014 PracticeCoding. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KPRTest : NSObject
+
+@end
---
+++ Git Test/KPRTest.m
@@ -0,0 +1,13 @@
+//
+//  KPRTest.m
+//  Git Test
+//
+//  Created by Kyle Rand on 8/19/14.
+//  Copyright (c) 2014 PracticeCoding. All rights reserved.
+//
+
+#import "KPRTest.h"
+
+@implementation KPRTest
+
+@end
